b78fd05c76758dd86b5b42f0c5c17820d9a873c1
Pablo Danilo <pperezg05@larioja.edu.es>
2024-03-20 16:39:36 +0100

    inicializar directorio proyecto

---
+++ src/ud4_git_hub_holamundonb/Ud4_Git_Hub_HolaMundoNB.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ud4_git_hub_holamundonb;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Ud4_Git_Hub_HolaMundoNB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
